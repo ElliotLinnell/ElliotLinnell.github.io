6932c1b59c40169ac77555eb2e3a61576ea089c8
Elliot Linnell <Linnellelliot@gmail.com>
2024-06-05 09:30:27 +0100

    gf

--- submit.php
+++ submit.php
@@ -1,22 +1,20 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
-    
-    $to = "linnellelliot@gmail.com";
-    $subject = "New Form Submission";
-    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
-    $headers = "From: $email";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = htmlspecialchars($_POST['name']);
+        $email = htmlspecialchars($_POST['email']);
+        $message = htmlspecialchars($_POST['message']);
+        
+        $to = "linnellelliot@gmail.com";
+        $subject = "New Form Submission";
+        $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+        $headers = "From: $email";
 
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Email successfully sent.";
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Email successfully sent.";
+        } else {
+            echo "Email sending failed.";
+        }
     } else {
-        echo "Email sending failed.";
+        echo "An error occurred.";
     }
-} else {
-    echo "An error occurred.";
-}
 ?>
-
-
